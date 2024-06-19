68d9e516eb2a26bf3a9fda8590136b6c611219bb
root <root@vultr.guest>
2024-06-19 09:27:33 +0000

    fix: filter dest coin is usdt

--- server/api/services/telegram.service.ts
+++ server/api/services/telegram.service.ts
@@ -6,18 +6,18 @@ export class TelegramService {
   async swapMsgSender(tokenDest: any) {
 
     if (tokenDest) {
-      // const tokenMetadataDest = await getTokenMetadata(tokenDest.mint)
+      const tokenMetadataDest = await getTokenMetadata(tokenDest.mint)
 
-      // const symbolDest = tokenMetadataDest ? tokenMetadataDest.symbol : "Can't get synbol metadata"
+      const symbolDest = tokenMetadataDest ? tokenMetadataDest.symbol : "Can't get synbol metadata"
   
-      // if (symbolDest != "SOL" && symbolDest != "USDT" && symbolDest != "USDC" ) {
+      if (symbolDest != "SOL" && symbolDest != "USDT" && symbolDest != "USDC" ) {
         const poolInfo = await getPoolInfo(tokenDest.mint)
         console.log("start to generate swap tx")
         const rayService = new RaydiumService()
         await rayService.txTracker(poolInfo.poolAddress)
         console.log("swap generate successfully")
          
-      // }
+      }
     }
   }
 }
